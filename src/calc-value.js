8c8b490176fea1b7dae90f47c0134f930c0a558d
Wolflan Freitas <wolflanf@gmail.com>
2022-12-07 16:08:46 -0300

    Testes completos

--- src/calc-value.js
+++ src/calc-value.js
@@ -6,12 +6,32 @@ function calcularMontante(capital, taxa, periodo) {
 function arredondar(valor) {
     // TODO
     const precisao = 100;
-    const arredondado = Math.round(valor * precisao) / precisao;
+    const arredondado = Math.round((valor + Number.EPSILON) * precisao) / precisao;
 
     return arredondado;
 }
 
+function calcularPrestacoes(montante, numeroPrestacoes) {
+    const prestacaoBase = arredondar(montante/numeroPrestacoes);
+    const resultado = Array(numeroPrestacoes).fill(prestacaoBase);
+
+    let somaPrestacoes = resultado.reduce((a, t) => a + t);
+    let diferenca = montante - somaPrestacoes;
+    const fator = diferenca > 0 ? 1 : -1;
+    let i = diferenca > 0 ? 0 : resultado.length - 1;
+
+    while(diferenca !== 0) {
+        resultado[i] = arredondar(resultado[i] + (0.01 * fator));
+        somaPrestacoes = resultado.reduce((a, t) => a + t);
+        diferenca = arredondar(montante - somaPrestacoes);
+        i += fator;
+    }
+
+    return resultado;
+}
+
 export {
     calcularMontante,
-    arredondar
+    arredondar,
+    calcularPrestacoes
 }
--- test/calc-value.test.js
+++ test/calc-value.test.js
@@ -1,4 +1,4 @@
-import {calcularMontante, arredondar} from "../src/calc-value.js"
+import {calcularMontante, arredondar, calcularPrestacoes} from "../src/calc-value.js"
 
 describe('calcularMontante', () => {
     test('Com uma prestação o montante é igual ao capital', () => {
@@ -20,5 +20,74 @@ describe('arredondar', () => {
     test('Arredondar em duas casas decimais', () => {
         const resultado = arredondar(538.4453124999998);
         expect(resultado).toBe(538.45);
+    });
+    test('1.005 deve retornar 1.01', () => {
+        const resultado = arredondar(1.005);
+        expect(resultado).toBe(1.01);
+    });
+});
+
+describe('calcularPrestacoes', () => {
+    test("O número de parcelas é igual ao número de prestações", () => {
+        const numeroPrestacoes = 6;
+        const prestacoes = calcularPrestacoes(200, numeroPrestacoes);
+        expect(prestacoes.length).toBe(numeroPrestacoes);
+    });
+
+    test('Um única prestação, valor igual ao montante', () => {
+        const numeroPrestacoes = 1;
+
+        const prestacoes = calcularPrestacoes(50, numeroPrestacoes);
+
+        expect(prestacoes.length).toBe(numeroPrestacoes);
+        expect(prestacoes[0]).toBe(50);
+    });
+
+    test("Duas prestações, valor igual à metade do montante", () => {
+        const numeroPrestacoes = 2;
+
+        const prestacoes =  calcularPrestacoes(50, numeroPrestacoes);
+
+        expect(prestacoes.length).toBe(numeroPrestacoes);
+        expect(prestacoes[0]).toBe(25);
+        expect(prestacoes[1]).toBe(25);
+    });
+
+    test('Valor da soma das prestações deve ser igual ao montante com duas casas decimais', () => {
+        // Dado (Given)
+        const numeroPrestacoes = 3;
+        const montante = 100;
+
+        // Quando (When)
+        const prestacoes = calcularPrestacoes(montante, numeroPrestacoes);
+
+        // Então (Then)
+        expect(prestacoes.length).toBe(numeroPrestacoes);
+        const soma = arredondar(prestacoes[0] + prestacoes[1] + prestacoes[2]);
+        expect(soma).toBe(montante);
+
+        for(let i = 0; i < prestacoes.length - 1; i++) {
+            const j = i + 1;
+            expect(prestacoes[i]).toBeGreaterThanOrEqual(prestacoes[j])
+        }
+    });
+
+    test('Desafio semi-final', () => {
+        //Given
+        const numeroPrestacoes = 3;
+        const montante = 101.994;
+
+        // Quando (When)
+        const prestacoes = calcularPrestacoes(montante, numeroPrestacoes);
+
+        // Então (Then)
+        expect(prestacoes.length).toBe(numeroPrestacoes);
+        const soma = arredondar(prestacoes[0] + prestacoes[1] + prestacoes[2]);
+        expect(soma).toBe(arredondar(montante));
+
+        for(let i = 0; i < prestacoes.length - 1; i++) {
+            const j = i + 1;
+            expect(prestacoes[i]).toBeGreaterThanOrEqual(prestacoes[j]);
+        }
     });
 });
